83dfd786e3429d154cdc013e91dd0490260e60e6
me <bhavesh.pareek36@gmail.com>
2020-03-29 19:14:51 +0530

    move jsx-runtime to root

--- build.py
+++ build.py
@@ -7,17 +7,17 @@ import os
 OUTPUT_FILE_PATH_DEFAULT = "./dist/ui-lib.modern.js"
 OUTPUT_FILE_SET = "./dist/ui-lib.m.js"
 DOCS_FILE_PATH = "./docs/ui/index.js"
-JSX_RUNTIME_FOLDER_OUTPUT = "./dist/jsx-runtime"
-JSX_RUNTIME_SRC = "./src/jsx-runtime"
+# JSX_RUNTIME_FOLDER_OUTPUT = "./dist/jsx-runtime"
+# JSX_RUNTIME_SRC = "./src/jsx-runtime"
 
 
 def run():
     subprocess.Popen(["npm", "run", "microbundle-start"]).wait()
     shutil.copyfile(OUTPUT_FILE_PATH_DEFAULT, DOCS_FILE_PATH)
     os.rename(OUTPUT_FILE_PATH_DEFAULT, OUTPUT_FILE_SET)
-    if os.path.isdir(JSX_RUNTIME_FOLDER_OUTPUT):
-        shutil.rmtree(JSX_RUNTIME_FOLDER_OUTPUT)
-    shutil.copytree(JSX_RUNTIME_SRC, JSX_RUNTIME_FOLDER_OUTPUT)
+    # if os.path.isdir(JSX_RUNTIME_FOLDER_OUTPUT):
+    #     shutil.rmtree(JSX_RUNTIME_FOLDER_OUTPUT)
+    # shutil.copytree(JSX_RUNTIME_SRC, JSX_RUNTIME_FOLDER_OUTPUT)
 
 
 if __name__ == "__main__":
