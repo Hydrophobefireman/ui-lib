990773c0f3050fce477ded640c01b1045540e622
me <bhavesh.pareek36@gmail.com>
2020-07-08 00:27:51 +0530

    update typings for useCallback to accept arguments

--- src/hooks/implementation/useCallback.ts
+++ src/hooks/implementation/useCallback.ts
@@ -1,5 +1,8 @@
 import { useMemo } from "./useMemo";
 
-export function useCallback<T>(fn: () => T, dependencies?: any[]) {
+export function useCallback<T>(
+  fn: (...a: unknown[]) => T,
+  dependencies?: any[]
+) {
   return useMemo(() => fn, dependencies);
 }
