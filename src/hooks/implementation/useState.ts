78961b6bc553fed67c8d612e46ba39c90d719a1f
me <bhavesh.pareek36@gmail.com>
2020-07-21 20:31:30 +0530

    update typings for setState

--- src/hooks/implementation/useState.ts
+++ src/hooks/implementation/useState.ts
@@ -4,10 +4,13 @@ import { useReducer } from "./useReducer";
 // the useState hook
 // we accept either a value or a function (incase the value is expensive) as the argument
 // internally we simply use a useReducer hook
-type StateArg<T> = T | (() => T);
 
-export function useState<T>(
-  initialState: StateArg<T>
-): [T, (previousState?: StateArg<T>) => void] {
-  return useReducer(consumeCallable as any, initialState);
+type StateArg<T> = T | (() => T);
+type Updater<T> = (arg: T | ((previous?: T) => T)) => void;
+type SetStateHookReturn<T> = [T, (arg: T | Updater<T>) => void];
+
+export function useState<T>(initialState: StateArg<T>): SetStateHookReturn<T> {
+  return useReducer(consumeCallable as any, initialState) as SetStateHookReturn<
+    T
+  >;
 }
