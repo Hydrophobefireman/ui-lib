915304aef5aca23f2974c920a2c007b91ef2b9bd
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2021-10-08 02:51:02 -0400

    fix(types): hotfix fwdref

--- src/forward_ref.ts
+++ src/forward_ref.ts
@@ -1,17 +1,17 @@
 import { addPluginCallback } from "./config";
-import { Props, RefType } from "./types";
+import { ComponentChild, Props, RefType } from "./types";
 import { objectWithoutKeys } from "./util";
 
 type RefObj<R> = ((val: R) => void) | RefType<R>;
 export interface ForwardFn<P = {}, T = any> {
-  (props: P, ref: RefObj<T>): JSX.Element | null | undefined | boolean;
+  (props: P, ref: RefObj<T>): ComponentChild;
 }
 export function forwardRef<P = Props<{ ref?: any }>, R = any>(C: ForwardFn) {
-  function ForwardRef(props: P) {
+  function ForwardRef(props: P): JSX.Element {
     const cloned = objectWithoutKeys(props, ["ref"] as any);
     // check if this can cause problems with hooks or not
     // TODO: maybe have a version that does it implicitly too?
-    return C(cloned, (props as any).ref);
+    return C(cloned, (props as any).ref) || (null as any);
   }
   ForwardRef.__REF_FORWARDED = true;
   return ForwardRef;
